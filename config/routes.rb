0dc637cfd928cf15e5e80f4cb8dd9ffe1bc65d78
Matthew Giardina <mlgiardina@me.com>
2015-06-03 15:23:35 -0500

    fixed issue with root

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
     put '/todos/:id/', to: 'todos#update'
 
-    root 'students#index'
+    root 'todos#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
