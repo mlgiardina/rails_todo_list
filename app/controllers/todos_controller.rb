120db288ec78b00bf1caa01321f858311aaf3ce4
Matthew Giardina <mlgiardina@me.com>
2015-06-03 14:17:39 -0500

    navigating to a user that doesn't exist now gives an error as json

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,7 +10,13 @@ class TodosController < ApplicationController
   end
 
   def show
-    render json: Todo.find(params[:id])
+    begin
+      render json: Todo.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => error
+      render json: { error: error.message }, status: 404
+    rescue StandardError => error
+      render json: { error: error.message }, status: 422
+    end
   end
 
 end
